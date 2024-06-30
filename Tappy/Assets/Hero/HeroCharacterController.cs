8e3f1eee97396b44f49e664e745475db9c08fcc8
DiegoPreza906 <103480030+DiegoPreza906@users.noreply.github.com>
2024-06-29 18:45:54 -0600

    Primer Avance Tappy
    Run Controller

---
+++ Tappy/Assets/Hero/HeroCharacterController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCharacterController : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayer;
+    private float gravity = -50f;
+
+    private CharacterController characterController;
+    private Vector3 velocity;
+
+    //Check Ground
+    private bool isGrounded;
+
+    //Horizontal Move
+    private float horizontalInput;
+
+    //Multiple Speed
+    [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private float jumpHeigh = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = 1;
+
+        transform.forward = new Vector3(horizontalInput, 0, Mathf.Abs(horizontalInput) - 1);
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayer, QueryTriggerInteraction.Ignore);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            Debug.Log("Hay píso");
+            velocity.y = 0;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        characterController.Move(new Vector3(horizontalInput * runSpeed, 0, 0) * Time.deltaTime);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y += Mathf.Sqrt(jumpHeigh * -2 * gravity);
+        }
+        /*
+        if (isGrounded && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeigh * -2 * gravity);
+        }
+        */
+        characterController.Move(velocity * Time.deltaTime);
+
+
+    }
+}
