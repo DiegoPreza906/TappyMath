74a3542cc39060dedf40697ae4789495516a6eb8
DiegoPreza906 <103480030+DiegoPreza906@users.noreply.github.com>
2024-07-09 08:01:17 -0600

    Cambio de camara

--- Tappy/Assets/Dobby/Scrips/CamaraFollow.cs
+++ Tappy/Assets/Dobby/Scrips/CamaraFollow.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class CamaraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target = null;
-
+    private bool playOn = false;
     private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Inicializa la posición de la cámara pegada al objetivo
         offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x,1,target.position.z)+offset, Time.deltaTime*4);
+        if (playOn)
+        {
+            // Sigue al objetivo desde atrás
+            Vector3 targetPosition = target.position + new Vector3(0, 1, -1); // Ajusta estos valores según lo necesites
+            transform.position = Vector3.Lerp(transform.position, targetPosition + offset, Time.deltaTime * 4);
+        }
     }
 }
